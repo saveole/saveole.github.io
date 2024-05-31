274c5d9b79cf102a0771f7d0da43200bbe5b4cc6
saveole <xgz.cz@foxmail.com>
2024-05-31 17:29:20 +0800

    config:avatar + contact info

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1717122277',resources: [
+    cacheName: 'chirpy-1717147700',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
