6baeeaef28b5cbaea37ce6de751b5848bce78d0f
saveole <xgz.cz@foxmail.com>
2024-06-01 16:40:32 +0800

    config:custome favicon

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1717229380',resources: [
+    cacheName: 'chirpy-1717231153',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
