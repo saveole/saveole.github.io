34700e1a94fcd731691cc13785dbbc3f0a1acf16
saveole <xgz.cz@foxmail.com>
2024-06-01 17:17:05 +0800

    feature:add giscus as comment system

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1717231153',resources: [
+    cacheName: 'chirpy-1717233348',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
