9d7b3970d5d39c63d20fa00aa68c550ab7552dba
saveole <17688703451@163.com>
2025-03-21 15:56:23 +0800

    fix unreachable link

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742518531',resources: [
+    cacheName: 'chirpy-1742543716',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
